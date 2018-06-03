9095a86373dae9dae233ffb3b538a0813610e7af
William Chandler <chndlr.wllm@gmail.com>
2018-06-02 19:12:00 -0500

    implementing filter functions; updated styling on nav, footer

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -8,22 +8,28 @@ export class AnimalService {
   public animals: Observable<any[]>;
   public allAnimals: Observable<any[]>;
   public activeAnimals: Observable<any[]>;
+  public animalsByAge: Observable<any[]>;
 
   constructor(private db: AngularFirestore) {
     this.getAllAnimals();
     this.getActiveAnimals();
-  }
-
-  getAnimalById(id) {
-    this.animals = this.db.collection('/animals', ref => ref.where('id', '==', id)).valueChanges();
+    this.getAnimalsByAge();
   }
 
   getAllAnimals() {
     this.allAnimals = this.db.collection('/animals').valueChanges();
   }
 
+  getAnimalById(id) {
+    this.animals = this.db.collection('/animals', ref => ref.where('id', '==', id)).valueChanges();
+  }
+
   getActiveAnimals() {
     this.activeAnimals = this.db.collection('/animals', ref => ref.where('profile_active', '==', true)).valueChanges();
+  }
+
+  getAnimalsByAge() {
+    this.animalsByAge = this.db.collection('/animals', ref => ref.where('dob', '==', '')).valueChanges();
   }
 
 }
