8ece1a785deb7a57483f4b6981a6060c8e6912e8
William Chandler <chndlr.wllm@gmail.com>
2018-06-02 21:00:18 -0500

    creating master query for filters

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -5,43 +5,51 @@ import { Animal } from '../models/animal';
 
 @Injectable()
 export class AnimalService {
-
   private itemsCollection: AngularFirestoreCollection<Animal>;
   public animals: Observable<any[]>;
   public allAnimals: Observable<any[]>;
   public activeAnimals: Observable<any[]>;
   public animalsByAge: Observable<any[]>;
+  public animalsBySex: Observable<any[]>;
 
   constructor(private db: AngularFirestore) {
-    this.itemsCollection = this.db.collection('animals');
+    this.itemsCollection = this.db.collection("animals");
     this.getAllAnimals();
     this.getActiveAnimals();
-    this.getAnimalsByAge();
   }
 
   getAllAnimals() {
-    this.allAnimals = this.db.collection('/animals').valueChanges();
+    this.allAnimals = this.db.collection("/animals").valueChanges();
   }
 
   getAnimalById(id) {
-    this.animals = this.db.collection<Animal>('/animals', ref => ref.where('id', '==', id)).valueChanges();
+    this.animals = this.db
+      .collection<Animal>("/animals", ref => ref.where("id", "==", id))
+      .valueChanges();
   }
 
   getAllAnimals() {
-    this.allAnimals = this.db.collection<Animal>('/animals').valueChanges();
+    this.allAnimals = this.db.collection<Animal>("/animals").valueChanges();
   }
 
   getActiveAnimals() {
-    this.activeAnimals = this.db.collection<Animal>('/animals', ref => ref.where('profile_active', '==', true)).valueChanges();
+    this.activeAnimals = this.db
+      .collection<Animal>("/animals", ref =>
+        ref.where("profile_active", "==", true)
+      )
+      .valueChanges();
   }
 
-  addAnimal(animal: Animal){
+  addAnimal(animal: Animal) {
     // this.db.collection<Animal>('/animals').add(animal);
     this.itemsCollection.add(JSON.parse(JSON.stringify(animal)));
   }
 
-  getAnimalsByAge() {
-    this.animalsByAge = this.db.collection('/animals', ref => ref.where('dob', '==', '')).valueChanges();
+  filterAnimals(query: any) {
+    this.activeAnimals = this.db
+      .collection<Animal>("/animals", ref => ref
+          .where("ages", "<=", query.maxAge)
+          .where("ages", ">=", query.minAge)
+      .valueChanges();
   }
-
 }
